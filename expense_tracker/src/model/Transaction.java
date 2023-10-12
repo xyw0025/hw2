1e3b6ca8e5ffaa6487d7b95f2950d631f50caa5c
Anshumaan-Chauhan02 <anshjan4@gmail.com>
2023-10-12 12:02:04 -0400

    fix: declaration type Transaction class variables

--- expense_tracker/src/model/Transaction.java
+++ expense_tracker/src/model/Transaction.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 public class Transaction {
 
-  public double amount;
-  public String category;
-  public String timestamp;
+  private double amount;
+  private String category;
+  private String timestamp;
 
   public Transaction(double amount, String category) {
     this.amount = amount;
