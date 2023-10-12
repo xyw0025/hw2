a61f31ae8928059994a30d55aae7f341beff04f0
Anshumaan-Chauhan02 <anshjan4@gmail.com>
2023-10-12 12:05:27 -0400

    fix: Adhered to Immutability by removing setter methods

--- expense_tracker/src/model/Transaction.java
+++ expense_tracker/src/model/Transaction.java
@@ -20,16 +20,8 @@ public class Transaction {
     return amount;
   }
 
-  public void setAmount(double amount) {
-    this.amount = amount;
-  }
-
   public String getCategory() {
     return category;
-  }
-
-  public void setCategory(String category) {
-    this.category = category; 
   }
   
   public String getTimestamp() {
