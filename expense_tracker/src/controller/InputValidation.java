1c58e0764237e8e4ffbdf4931cd7148fa2320f24
Anshumaan-Chauhan02 <anshjan4@gmail.com>
2023-10-12 20:43:27 -0400

    doc: Added documentation for InputValidation

--- expense_tracker/src/controller/InputValidation.java
+++ expense_tracker/src/controller/InputValidation.java
@@ -2,8 +2,15 @@ package controller;
 
 import java.util.Arrays;
 
+/**
+ * Class to validate the inputs entered by the user in the Input fields
+ */
 public class InputValidation {
 
+  /**
+   * @param amount value entered in the amount field by the user
+   * @return boolean indicating whether the value adheres by the rules or not
+   */
   public static boolean isValidAmount(double amount) {
     
     // Check range
@@ -19,6 +26,10 @@ public class InputValidation {
     return true;
   }
 
+  /**
+   * @param category category entered by the user in the category input field
+   * @return boolean value indicating whether it falls into the list of allowed  categories
+   */
   public static boolean isValidCategory(String category) {
 
     if(category == null) {
