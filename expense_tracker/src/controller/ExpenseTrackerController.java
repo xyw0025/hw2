6d7de0b0c1315ec3899353d812aa9d1c473bc61c
Anshumaan-Chauhan02 <anshjan4@gmail.com>
2023-10-13 11:16:06 -0400

    feat: applyFilter functionality

--- expense_tracker/src/controller/ExpenseTrackerController.java
+++ expense_tracker/src/controller/ExpenseTrackerController.java
@@ -1,7 +1,8 @@
 package controller;
 
 import view.ExpenseTrackerView;
-
+import strategy.AmountFilter;
+import strategy.CategoryFilter;
 import java.util.List;
 
 
@@ -51,5 +52,31 @@ public class ExpenseTrackerController {
     view.deleteTransactionRow(index_number); // the row number also start from 0
     refresh();
     return true;
+  }
+
+  public boolean applyFilter(double filterAmount, String filterCategory)
+  {
+    if (filterCategory==null)
+    {
+      if (!InputValidation.isValidAmount(filterAmount)) {
+      return false;
+      }
+      List<Transaction> filteredTransactions = new AmountFilter(filterAmount).filter(model.getTransactions());
+      view.setBackgroundColor(filteredTransactions);
+    }
+    
+    else if (filterAmount==0)
+    {
+      if (!InputValidation.isValidCategory(filterCategory)) {
+      return false;
+    }
+      List<Transaction> filteredTransactions = new CategoryFilter(filterCategory).filter(model.getTransactions());
+      view.setBackgroundColor(filteredTransactions);
+    }
+    else
+    {
+      return false;
+    } 
+  return true;
   }
 }
