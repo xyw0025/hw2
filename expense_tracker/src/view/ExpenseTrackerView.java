32aa90d7024f84aad66ca79f0967c51e054b0beb
Anshumaan-Chauhan02 <anshjan4@gmail.com>
2023-10-12 21:41:07 -0400

    doc: Added javadoc for ExpenseTrackerView

--- expense_tracker/src/view/ExpenseTrackerView.java
+++ expense_tracker/src/view/ExpenseTrackerView.java
@@ -12,16 +12,40 @@ import java.text.NumberFormat;
 import model.Transaction;
 import java.util.List;
 
+/**
+ * Class containing all the components present in the GUI
+ */
 public class ExpenseTrackerView extends JFrame {
 
+  /**
+   * JTable object used to display and edit cells
+   */
   private JTable transactionsTable;
+  /**
+   * Button object to add transaction 
+   */
   private JButton addTransactionBtn;
+  /**
+   * Button object to delete a transaction
+   */
   private JButton deleteTransactionBtn;
+  /**
+   * Text field to enter amount present in the transaction
+   */
   private JFormattedTextField amountField;
+  /**
+   * Text field to enter category present in the transaction
+   */
   private JTextField categoryField;
+  /**
+   * Model that store the transaction values in the Table cells
+   */
   private DefaultTableModel model;
   
 
+  /**
+   * Constructor to initialize GUI components of the application
+   */
   public ExpenseTrackerView() {
     setTitle("Expense Tracker"); // Set title
     setSize(600, 400); // Make GUI larger
@@ -71,6 +95,10 @@ public class ExpenseTrackerView extends JFrame {
   
   }
 
+  /**
+   * Method to reinitialize the table contents using the passed transactions list
+   * @param transactions list containing transaction objects used to modify the view
+   */
   public void refreshTable(List<Transaction> transactions) {
       // Clear existing rows
       model.setRowCount(0);
@@ -96,18 +124,35 @@ public class ExpenseTrackerView extends JFrame {
   
 
   
-  
+  /**
+   * Function to access addTransaction button object
+   * @return a JButton object to add transactions
+   */
   public JButton getAddTransactionBtn() {
     return addTransactionBtn;
   }
+  
+  /**
+   * Function to access table model object
+   * @return DefaultTableModel object that stores the transaction values in the cells
+   */
   public DefaultTableModel getTableModel() {
     return model;
   }
   // Other view methods
-    public JTable getTransactionsTable() {
+  
+  /**
+   * Function to return the transactionsTable object 
+   * @return JTable object that is used to define the structure of table
+   */
+  public JTable getTransactionsTable() {
     return transactionsTable;
   }
 
+  /**
+   * Function to return the amount value in the amount TextField
+   * @return returns the amount value entered by the user
+   */
   public double getAmountField() {
     if(amountField.getText().isEmpty()) {
       return 0;
@@ -117,23 +162,43 @@ public class ExpenseTrackerView extends JFrame {
     }
   }
 
+  /**
+   * Function to set value to the amountField
+   * @param amountField default value to be assigned to the amountField
+   */
   public void setAmountField(JFormattedTextField amountField) {
     this.amountField = amountField;
   }
 
   
+  /**
+   * Function to return the category value in the amount TextField
+   * @return returns the category value entered by the user
+   */
   public String getCategoryField() {
     return categoryField.getText();
   }
 
+  /**
+   * Function to set value to the categoryField
+   * @param amountField default value to be assigned to the categoryField
+   */
   public void setCategoryField(JTextField categoryField) {
     this.categoryField = categoryField;
   }
 
+  /**
+   * Function to access deleteTransactionBtn button object
+   * @return a JButton object to delete transactions
+   */
   public JButton getDeleteTransactionBtn() {
     return deleteTransactionBtn;
   }
 
+  /**
+   * Function to delete/remove an indexed row from the table
+   * @param index_number Index of the transaction to be deleted in the table
+   */
   public void deleteTransactionRow(int index_number) {
     this.getTableModel().removeRow(index_number);
   }
