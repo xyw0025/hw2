763e146f68c1fbcfb3c9ee5b768a04ebe13ace7e
Anshumaan-Chauhan02 <anshjan4@gmail.com>
2023-10-12 20:50:19 -0400

    doc: Added javadoc for the ExpenseTrackerModel

--- expense_tracker/src/model/ExpenseTrackerModel.java
+++ expense_tracker/src/model/ExpenseTrackerModel.java
@@ -3,26 +3,47 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Model class performs maintains and updates the transactions list 
+ */
 public class ExpenseTrackerModel {
 
+  /**
+   * A list of Transaction objects 
+   */
   private List<Transaction> transactions;
 
+  /**
+   * Constructor initializing an empty transactions list
+   */
   public ExpenseTrackerModel() {
     transactions = new ArrayList<>(); 
   }
 
+  /**
+   * @param t A new transaction object to be added in the list
+   */
   public void addTransaction(Transaction t) {
     transactions.add(t);
   }
 
+  /**
+   * @param t Transaction object to be deleted from the list
+   */
   public void removeTransaction(Transaction t) {
     transactions.remove(t);
   }
 
+  /**
+   * @param index Index of the transaction to be removed from the list
+   */
   public void removeTransaction(int index) {
     transactions.remove(index);
   }
 
+  /**
+   * @return 
+   */
   public List<Transaction> getTransactions() {
     final List<Transaction> copyTransactions;
     Collections.copy(transactions, copyTransactions)
